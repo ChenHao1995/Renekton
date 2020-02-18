a9418b22e340e2126202aea4fb4f97d3c8d8f33f
陈浩 <chenhao@chenhaodeMacBook-Pro.local>
2020-02-18 16:24:07 +0800

    页面逻辑

--- miniprogram/pages/index-home/index-home.js
+++ miniprogram/pages/index-home/index-home.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+  onLoad: function(options) {
+    // 页面创建时执行
+  },
+  chooseList: function() {
+    wx.showActionSheet({
+      itemList: ['A', 'B', 'C'],
+      success(res) {
+        console.log(res.tapIndex)
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
+  }
+})
